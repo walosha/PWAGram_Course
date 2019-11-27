bd906689db4dc47947bb8f096e7d61cb348e6f44
walosha <walosha@yahoo.com>
2019-11-27 23:20:23 +0100

    Implemented  Service Workers - Caching api

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,14 +1,23 @@
-// Service Worker returns  a promise
-let deferredPrompt;
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").then(function() {
-    console.log("Service worker registered ...");
-  });
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function() {
+      console.log("Service worker registered!");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
 window.addEventListener("beforeinstallprompt", function(event) {
-  console.log("Before the beforeinstallprompt event is fired !");
+  console.log("beforeinstallprompt fired");
   event.preventDefault();
-  deferredPrompt = null;
+  deferredPrompt = event;
   return false;
 });
--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,59 @@
+let CACHE_STATIC_NAME = "Static-v2";
+let CACHE_DYNAMIC_NAME = "dynamic";
+
 self.addEventListener("install", function(event) {
-  console.log("[Web Workers] is running and now installed,,", event);
+  event.waitUntil(
+    // Adding Cache Versioning
+    caches.open(CACHE_STATIC_NAME).then(function(cache) {
+      console.log("[Service Worker] Precaching App sheel ...");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/css/feed.css",
+        "/src/css/app.css",
+        "/src/css/help.css",
+        "/src/js/material.min.js",
+        "/src/images/sf-boat.jpg",
+        "/src/images/main-image-lg.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+      ]);
+    })
+  );
 });
+
 self.addEventListener("activate", function(event) {
-  console.log("[Web Workers] Activated web workers ...", event);
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log("[Services Worker] is deleting old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
   return self.clients.claim();
 });
+
 self.addEventListener("fetch", function(event) {
-  console.log("[Web Workers] Fetching something ...", event);
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request).then(function(res) {
+          return caches.open(CACHE_DYNAMIC_NAME).then(function(cache) {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
+        });
+      }
+    })
+  );
 });
